d61f52e76406126d94b939474a320a8c26b7dc7d
Iliya Rahozin <yarusha4true@gmail.com>
2021-10-04 01:24:33 +0300

    first page

---
+++ script/main.js
@@ -0,0 +1,7 @@
+function changebg(){
+    document.getElementById("intro_cont_main-body").style.background = "url('../img/svg/start_bg_ak-47-inverse.jpg')";
+  }
+  
+  function resetbg(){
+    document.getElementById("intro_cont_main-body").style.background = "";
+  }
